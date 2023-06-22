7ea89a5f6bc8fbb3dab2f5ce23fd552822dea56b
Bakhtiyar <ia.esploristo@gmail.com>
2023-06-22 23:08:00 +0600

    L7

---
+++ L7Obj/js/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] options = {"test", "1024", "black", "red"};
+        for (String e : options) {
+            System.out.println(e);
+        }
+    }
+}
